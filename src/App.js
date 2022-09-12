b9628c726bb9e43648ccd85030557e7c6603faf7
Andre Bauer <andrebauer@live.com>
2022-09-12 16:33:35 +0900

    working with components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React from "react";
 
-const Bemvindo = () => <h2>Bem vindo(a)</h2>;
+const Bemvindo = (props) => {
+  return (
+    <div>
+      <h2>Bem vindo(a) {props.nome}</h2>
+      <h3>Tenho {props.idade} anos</h3>
+    </div>
+  );
+};
 
 function App() {
   return (
     <div>
       Ola mundo
-      <Bemvindo />
+      <Bemvindo nome="Matheus" idade="24" />
+      <Bemvindo nome="Amanda" idade="19" />
       <h1>Curso react</h1>
     </div>
   );
