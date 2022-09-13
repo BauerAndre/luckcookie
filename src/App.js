28bf8654f2015b245518158055f43785a65f085d
Andre Bauer <andrebauer@live.com>
2022-09-13 15:14:02 +0900

    using didMount didUpdate ...

--- src/App.js
+++ src/App.js
@@ -1,44 +1,29 @@
-import React, { Comment, Component } from "react";
+import React, { Component } from "react";
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      nome: "Matheus",
-      contador: 0,
+      hora: "00:00:00",
     };
-    this.aumentar = this.aumentar.bind(this);
-    this.diminuir = this.diminuir.bind(this);
   }
 
-  aumentar() {
-    let state = this.state;
-    state.contador += 1;
-    this.setState(state);
+  componentDidMount() {
+    setInterval(() => {
+      this.setState({ hora: new Date().toLocaleTimeString() });
+    }, 1000);
   }
 
-  diminuir() {
-    let state = this.state;
-    if (state.contador === 0) {
-      alert("Opa chegou a zero!");
-      return;
-    }
-    state.contador -= 1;
-    this.setState(state);
+  componentDidUpdate() {
+    console.log("atualizou!!");
   }
 
   render() {
     return (
       <div>
-        <h1>Contador</h1>
-        <h3>
-          <button onClick={this.diminuir}>-</button>
-          {this.state.contador}
-          <button onClick={this.aumentar}>+</button>
-        </h3>
+        <h1>Meu projeto {this.state.hora}</h1>
       </div>
     );
   }
 }
-
 export default App;
